2a6f41f8baec77cd55c989ec520e2e71e4d9e336
murilodamarioo <murilo.gcdamario@gmail.com>
2025-01-01 18:58:26 -0300

    feat:  implement profile test E2E

--- src/http/controllers/profile.ts
+++ src/http/controllers/profile.ts
@@ -8,5 +8,5 @@ export async function profile(request: FastifyRequest, reply: FastifyReply) {
 
   const { user } = await getUserProfile.execute({ userId: request.user.sub })
 
-  return reply.status(200).send({ ...user, password_hash: undefined })
+  return reply.status(200).send({ user: { ...user, password_hash: undefined } })
 }
