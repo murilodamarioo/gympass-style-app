46ef493d4e867ade2878585e1f5bf6959081250c
murilodamarioo <murilo.gcdamario@gmail.com>
2025-01-01 21:45:17 -0300

    fix: get query from request.query

--- src/http/controllers/gyms/search.ts
+++ src/http/controllers/gyms/search.ts
@@ -8,7 +8,7 @@ export async function search(request: FastifyRequest, reply: FastifyReply) {
     page: z.coerce.number().min(1).default(1)
   })
 
-  const { query, page } = createGymQuerySchema.parse(request.body)
+  const { query, page } = createGymQuerySchema.parse(request.query)
 
   const searchGymsuseCase = makeSearchGymsUseCase()
 
