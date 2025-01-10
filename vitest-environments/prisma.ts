7fbe914503439627464e512182dde690d39ca58c
murilodamarioo <murilo.gcdamario@gmail.com>
2025-01-10 17:08:17 -0300

    fix: environment test

--- vitest-environments/prisma.ts
+++ vitest-environments/prisma.ts
@@ -1,9 +1,11 @@
 import 'dotenv/config'
-import { randomUUID } from "crypto";
-import { execSync } from 'child_process';
-import { Environment } from "vitest";
-import { prisma } from '../src/lib/prisma';
 
+import { randomUUID } from 'node:crypto'
+import { execSync } from 'node:child_process'
+import { Environment } from 'vitest'
+import { PrismaClient } from '@prisma/client'
+
+const prisma = new PrismaClient()
 
 function generateDataBaseURL(schema:string){
 
@@ -28,14 +30,16 @@ export default<Environment>{
     const schema = randomUUID()
 
     const databaseURL = generateDataBaseURL(schema)
-    
+
     process.env.DATABASE_URL = databaseURL
 
     execSync('npx prisma migrate deploy')
 
-    return{
-      async teardown(){
-        await prisma.$executeRawUnsafe(`DROP SCHEMA IF EXISTS "${schema} CASCADE"`)
+    return {
+      async teardown() {
+        await prisma.$executeRawUnsafe(
+          `DROP SCHEMA IF EXISTS "${schema}" CASCADE`
+        )
 
         await prisma.$disconnect()
       }
